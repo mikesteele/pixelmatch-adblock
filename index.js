a40e687b62b58d49b251319f1f2016a9d7b87bb1
Mike Steele <mike@steel.fm>
2020-07-10 22:54:09 -0400

    omg it works

--- index.js
+++ index.js
@@ -4,19 +4,41 @@ const htmlToImage = require('html-to-image');
 // Using webpack url-loader, we can load the reference image as a Base64 URI to re-create on the client
 import fbSponsoredPostReference from './fb.png';
 
-const htmlToImageElement = el => {
-  return new Promise(resolve => {
-    htmlToImage.toPng(el)
-      .then(dataUrl => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.src = dataUrl;
-      });
-  });
+const htmlToCanvas = el => {
+  return htmlToImage.toCanvas(el);
 };
 
-const doImageElementsMatch = (image1, image2) => {
-  // TODO - pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
+const doImageElementsMatch = (canvas1, canvas2) => {
+  // See https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/naturalWidth
+  const width = canvas1.width;
+  const height = canvas1.height;
+
+  // Create diff canvas
+  const canvas = document.createElement('canvas');
+  const diffCtx = canvas.getContext('2d');
+  const diff = diffCtx.createImageData(width, height);
+  const image1 = canvas1.getContext('2d').getImageData(0, 0, width, height);
+  const image2 = canvas2.getContext('2d').getImageData(0, 0, width, height);
+  pixelmatch(image1.data, image2.data, diff.data, width, height, { threshold: 0.1 });
+  diffCtx.putImageData(diff, 0, 0);
+  document.body.appendChild(canvas);
+
+  return true;
+};
+
+const createCanvasFromImageElement = image => {
+  // TODO - Does this work?
+  const width = image.naturalWidth;
+  const height = image.naturalHeight;
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.drawImage(image, 0, 0, width, height);
+
+  return canvas;
 };
 
 const createFacebookReferenceImage = () => {
@@ -27,14 +49,22 @@ const createFacebookReferenceImage = () => {
   });
 };
 
+const createFacebookReferenceCanvas = () => {
+  return new Promise(resolve => {
+    createFacebookReferenceImage()
+      .then(createCanvasFromImageElement)
+      .then(resolve);
+  });
+}
+
 const isFacebookSponsoredLink = el => {
   return new Promise(resolve => {
-    const createFacebookReferenceImagePromise = createFacebookReferenceImage();
-    const htmlToImageElementPromise = htmlToImageElement(el);
-    Promise.all([createFacebookReferenceImagePromise, htmlToImageElementPromise])
+    const createFacebookReferenceCanvasPromise = createFacebookReferenceCanvas();
+    const htmlToCanvasPromise = htmlToCanvas(el);
+    Promise.all([createFacebookReferenceCanvasPromise, htmlToCanvasPromise])
       .then(images => {
         images.forEach(image => document.body.appendChild(image));
-        //resolve(doImageElementsMatch(images[1], images[2]));
+        resolve(doImageElementsMatch(images[0], images[1]));
       });
   });
 };
